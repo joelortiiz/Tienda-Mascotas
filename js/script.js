d34b9b44ab8d08d5fcdc59ec8036459f4fcde5a9
joelortiiz <daw_joelortiz@riberadeltajo.es>
2023-11-24 15:44:51 +0100

    2 page

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 let logo = document.getElementById("logo");
+const title = document.getElementById("title");
+const titlecontainer = document.getElementById("container_title");
+const article = document.getElementById("article");
 
 const animationLogo =()=> {
     logo.classList.add("logo__animation");
@@ -6,6 +9,14 @@ const animationLogo =()=> {
 const animationLogoQuit =()=> {
     logo.classList.remove("logo__animation");
 }
+const animationTitleColor =()=> {
+    title.style.animationName("titleanimation2");
+}
+const animationArticleLoop =()=> {
+    // article.classList.add("animationloop");
+}
 
 logo.addEventListener("click", animationLogo)
 logo.addEventListener("animationend", animationLogoQuit)
+titlecontainer.addEventListener("animationstart", animationTitleColor)
+article.addEventListener("animationend", animationArticleLoop)
