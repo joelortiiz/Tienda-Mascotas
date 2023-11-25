3dd715ed885aa4f1a8dfcbd875dea48d2f4341bc
joelortiiz <daw_joelortiz@riberadeltajo.es>
2023-11-25 20:12:09 +0100

    Working on 3º html file

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ let logo = document.getElementById("logo");
 const title = document.getElementById("title");
 const titlecontainer = document.getElementById("container_title");
 const article = document.getElementById("article");
+const logoRazas = document.getElementById("bop");
+const logocat = document.getElementById("logocat");
 
 const animationLogo =()=> {
     logo.classList.add("logo__animation");
@@ -15,8 +17,18 @@ const animationTitleColor =()=> {
 const animationArticleLoop =()=> {
     // article.classList.add("animationloop");
 }
-
+const animationlogocat =()=> {
+    console.log("lao")
+    logoRazas.classList.remove("logocatanimation-display")
+    logoRazas.classList.add("logocatanimation")
+}
+const deleteAnimationcat =()=> {
+    logoRazas.classList.remove("logocatanimation")
+    logoRazas.classList.add("logocatanimation-display")
+}
 logo.addEventListener("click", animationLogo)
 logo.addEventListener("animationend", animationLogoQuit)
 titlecontainer.addEventListener("animationstart", animationTitleColor)
-article.addEventListener("animationend", animationArticleLoop)
+//article.addEventListener("animationend", animationArticleLoop)
+logocat.addEventListener("click", animationlogocat)
+logoRazas.addEventListener("animationend", deleteAnimationcat)
